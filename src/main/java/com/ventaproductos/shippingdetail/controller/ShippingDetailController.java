40f42a3bc4554bf48b6d9b139b0d864f1c4043c9
Ferchox <99991323+ProgramerSerchx@users.noreply.github.com>
2024-03-31 13:03:12 -0500

    Shipping Detail controller almost done

---
+++ src/main/java/com/ventaproductos/shippingdetail/controller/ShippingDetailController.java
@@ -0,0 +1,68 @@
+package com.ventaproductos.shippingdetail.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ventaproductos.shippingdetail.entity.ShippingDetailDTO;
+import com.ventaproductos.shippingdetail.service.ShippingdetailServiceImp;
+
+@RestController
+@RequestMapping("/api/v1/shipping")
+public class ShippingDetailController {
+    private final ShippingdetailServiceImp shippingDetail;
+
+    public ShippingDetailController(ShippingdetailServiceImp shippingDetail) {
+        this.shippingDetail = shippingDetail;
+    }
+
+    @GetMapping("/{id} ")
+    public ResponseEntity<Optional<ShippingDetailDTO>> get(@PathVariable("id") Integer id){
+        return new ResponseEntity<>(shippingDetail.get(id),HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ShippingDetailDTO>> getAll(){
+        return new ResponseEntity<>(shippingDetail.getAll(),HttpStatus.OK);
+    }
+
+    // @GetMapping("/{orderId} ") -----> Implementar método getByOrderById
+    // public ResponseEntity<Optional<ShippingDetailDTO>> getByOrderById(@PathVariable("orderId") Integer orderId){
+    //     return new ResponseEntity<>(shippingDetail.getByOrderById(orderId),HttpStatus.OK);
+    // }
+
+    @GetMapping("/conveyor/{conveyor}")
+    public ResponseEntity<List<ShippingDetailDTO>> getByConveyor(@PathVariable("conveyor") String conveyor){
+        return new ResponseEntity<>(shippingDetail.getByConveyor(conveyor),HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ShippingDetailDTO> create(@RequestBody ShippingDetailDTO dto) throws Exception{
+        shippingDetail.create(dto);
+        return new ResponseEntity<>(dto,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ShippingDetailDTO> update(@PathVariable("id") Integer id, @RequestBody ShippingDetailDTO dto) {
+        shippingDetail.update(id, dto);
+        return new ResponseEntity<>(dto,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
+        shippingDetail.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    
+}
