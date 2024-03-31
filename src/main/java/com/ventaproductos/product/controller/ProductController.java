7bea991b3eb984f8a093f5b5599c86e7df226c14
Ferchox <99991323+ProgramerSerchx@users.noreply.github.com>
2024-03-30 23:42:31 -0500

    pjs work

--- src/main/java/com/ventaproductos/product/controller/ProductController.java
+++ src/main/java/com/ventaproductos/product/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.ventaproductos.product.controller;
 
 import java.util.*;
+import java.math.*;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
