f8a61bfad41bd3be2b025abc124177c48c6c130f
Eliecer Ureche <elieceru.job@gmail.com>
2024-04-03 23:02:47 -0500

    Test ClientService and ProductService done

--- src/test/java/com/ventaproductos/TestServices/TestPaymentService.java
+++ src/test/java/com/ventaproductos/TestServices/TestPaymentService.java
@@ -2,14 +2,9 @@ package com.ventaproductos.TestServices;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
 import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
