1db7961299eba0e2490c17696b8a391d5b710f65
EliecerUJob <153546460+EliecerUJob@users.noreply.github.com>
2024-04-03 23:05:40 -0500

    Update TestPaymentService.java

--- src/test/java/com/ventaproductos/TestServices/TestPaymentService.java
+++ src/test/java/com/ventaproductos/TestServices/TestPaymentService.java
@@ -2,10 +2,7 @@ package com.ventaproductos.TestServices;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+
 import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDate;
